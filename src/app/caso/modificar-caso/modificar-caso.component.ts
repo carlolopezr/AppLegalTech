ff4b67a5fd8648a85efb3ee785bbb20ac3f24e1b
Carlos <carlo.lopezr@duocuc.cl>
2022-04-17 17:35:15 -0400

    Modificar Caso

--- src/app/caso/modificar-caso/modificar-caso.component.ts
+++ src/app/caso/modificar-caso/modificar-caso.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { caso } from 'src/app/interfaces';
 import { BasedatosService } from '../../services/basedatos.service';
 import { ActivatedRoute } from '@angular/router';
-import { demanda, tpDemanda, comuna, usuario, estado } from '../../interfaces';
+import { demanda, tpDemanda, comuna, usuario, estado, casoModificar } from '../../interfaces';
 
 @Component({
   selector: 'app-modificar-caso',
@@ -21,9 +21,9 @@ export class ModificarCasoComponent implements OnInit {
       id_estado:1,
       desc_estado:'Activo'
     }
-
   ];
 
+  abogados:usuario[]=[]
 
   // usuario:usuario={
   //   id_usuario:0,
@@ -45,28 +45,35 @@ export class ModificarCasoComponent implements OnInit {
     estado:''
   };
 
+  casoModificar:casoModificar={
+    id_caso:'',
+    usuario_id_usuario: '',
+    seguimiento: '',
+    detalle_caso:'',
+    estado:''
+  }
+
   constructor(private db: BasedatosService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
 
-    // Obtener casos
     this.db.getCasos().subscribe((casos) => {
       this.casos = casos;
       this.activatedRoute.params
         .subscribe(({ id }) => {
           this.casos.forEach(caso => {
             if (caso.id_caso == id) {
+              
               this.caso = caso;
+              this.caso.estado = caso.estado
               this.obtDemandas();
               this.obtComunas();  
-              this.obtTpDemandas();
-              // this.obtAbogado();                    
+              this.obtTpDemandas();  
+              this.obtAbogados();               
             }
           });
         })
     })
-
-
   }
 
   obtDemandas(){
@@ -83,24 +90,80 @@ export class ModificarCasoComponent implements OnInit {
 
   obtComunas(){
     this.db.getComunas().subscribe(comunas => {
-      this.comunas = comunas
-      console.log(this.comunas);
-      
+      this.comunas = comunas    
     })
   }
 
   obtTpDemandas(){
     this.db.getTipoDemandas().subscribe(tpDemandas => {
-      this.tpDemandas = tpDemandas
-      console.log(this.tpDemandas);
-      
+      this.tpDemandas = tpDemandas    
     })
   }
 
-  // obtAbogado(){
-  //   this.db.getUsuario(1).subscribe(usuario =>{
-  //     console.log(usuario);     
-  //   })
-  // }
+  obtAbogados(){
+    this.db.getAbogados().subscribe(abogados =>{
+      this.abogados = abogados
+    })  
+  }
+
+
+  modificarCaso(){
+
+    let estadoCaso:number=0;
+    let id_abogado:number = 0;
+
+    let estadoInput:string = (<HTMLInputElement>document.getElementById('estado')).value
+    let seguimientoInput:string = (<HTMLInputElement>document.getElementById('seguimiento')).value
+    
+
+    this.estados.forEach(estado => {
+      if (estadoInput == estado.desc_estado) {
+        estadoCaso = estado.id_estado
+      }
+    })
+
+    this.abogados.forEach(abogado => {
+      if (this.caso.nombre_usuario == abogado.nombre_usuario) {
+        id_abogado = abogado.id_usuario
+      }
+    });
+
+    this.casoModificar.id_caso = this.caso.id_caso
+    this.casoModificar.detalle_caso = this.caso.detalle_caso;
+    this.casoModificar.estado = estadoCaso.toString();
+    this.casoModificar.usuario_id_usuario = id_abogado.toString();
+    this.casoModificar.seguimiento = seguimientoInput
+
+    if (this.validarCaso(this.casoModificar)) {
+      this.db.putCaso(this.casoModificar).subscribe(datos => {     
+      });
+    }
+    else{
+      alert('Por favor complemente todos los campos')
+    }
+  }
+
+  validarCaso(caso:casoModificar):boolean{
+
+    let valido:boolean=true;
+    if(caso.detalle_caso==''){
+      valido=false
+    }
+    else if(caso.id_caso == ''){
+      valido=false
+    }
+    else if(caso.seguimiento==''){
+      valido=false
+    }
+    else if(caso.estado == ''){
+      valido=false
+    }
+    return valido  
+  }
+
+  modificarDemanda(){
+    
+  }
 
 }
+
